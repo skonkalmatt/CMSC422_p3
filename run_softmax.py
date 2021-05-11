d6caa0c11ee6518b43d41be1790ce4b9c7983316
skonkalmatt <skonkalmatt517@gmail.com>
2021-05-11 14:23:58 -0400

    code for QSR4

--- run_softmax.py
+++ run_softmax.py
@@ -1,5 +1,7 @@
 from utils import *
 from softmax import *
+from runClassifier import *
+import matplotlib.pyplot as plt
 
 # Code adapted from https://github.com/jatinshah/ufldl_tutorial
 
@@ -13,8 +15,18 @@ reg = 0.0001
 
 X, Y = loadMNIST('data/train-images.idx3-ubyte', 'data/train-labels.idx1-ubyte')
 sm = SoftmaxRegression(numClasses, exSize)
-sm.train(X, Y)
+#sm.train(X, Y)
 
 testX, testY = loadMNIST('data/t10k-images.idx3-ubyte', 'data/t10k-labels.idx1-ubyte')
 predictions = sm.predict(X)
 print("Accuracy: {0:.2f}%".format(100 * np.sum(predictions == Y, dtype=np.float64) / Y.shape[0]))
+print("*****************************************")
+(dataSizes, trainAcc, testAcc) = learningCurve(sm, numClasses, exSize, X, Y, testX, testY)
+print(dataSizes)
+plt.plot(trainAcc, 'ro')
+plt.plot(testAcc, 'bs')
+plt.ylabel('Accuracy')
+plt.xlabel('Examples Seen (per 60000)')
+plt.legend(['Training Accuracy', 'Test Accuracy'])
+plt.title('Training and Test Accuracy vs. Examples Seen')
+plt.show()
